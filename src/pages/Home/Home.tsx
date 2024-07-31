f13984c17810804632f820aa1f079b66a61adff0
Miguel Evangelista <miguelbranagan30@gmail.com>
2024-07-30 20:08:07 -0400

    testing background with vercel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,14 @@ import { Card } from "./components/Card";
 import Stage from "./components/Stage";
 export default function Home() {
   return (
-    <div className="pb-10 bg-[url('./background.svg')] bg-no-repeat bg-cover font-Quicksand">
-
+    <div className="pb-10 bg-[url('background.svg')] bg-no-repeat bg-cover font-Quicksand">
       {/* welcome section */}
-      <section className="h-screen w-full">
-        <div className="absolute z-50 w-1/2 h-full flex flex-col justify-center p-10 space-y-4">
-          <h1 className="font-Poppins text-primary-dark lg:text-9xl md:text-7xl">PETOPIA</h1>
-          <h3 className="font-Poppins text-primary-darker lg:text-4xl md:text-2xl font-bold">
+      <section className="w-full h-screen">
+        <div className="absolute z-50 flex flex-col justify-center w-1/2 h-full p-10 space-y-4">
+          <h1 className="font-Poppins text-primary-dark lg:text-9xl md:text-7xl">
+            PETOPIA
+          </h1>
+          <h3 className="font-bold font-Poppins text-primary-darker lg:text-4xl md:text-2xl">
             La app favorita de tu mascota
           </h3>
           <p>
@@ -19,14 +20,14 @@ export default function Home() {
             tengas el historial médico de tu mascota, ¡todo en un solo lugar!
             Porque tu mascota es parte de tu familia.
           </p>
-          <a className="font-Poppins text-primary-darker font-bold" href="#">
+          <a className="font-bold font-Poppins text-primary-darker" href="#">
             Registrarme
           </a>
         </div>
         <Stage />
       </section>
 
-      {/* care tips */} 
+      {/* care tips */}
       <section>
         {tipsList.map((element, inx) => (
           <Card
@@ -39,8 +40,8 @@ export default function Home() {
           />
         ))}
       </section>
-      <section className="flex w-full justify-center">
-        <button className="border border-primary-darker text-primary-darker rounded-lg p-4 font-bold">
+      <section className="flex justify-center w-full">
+        <button className="p-4 font-bold border rounded-lg border-primary-darker text-primary-darker">
           Mira más cuidados para tu mascota
         </button>
       </section>
